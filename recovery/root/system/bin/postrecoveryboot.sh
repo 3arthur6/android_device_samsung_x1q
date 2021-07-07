8a081695db9d3c6b3ebed08f5ada57d0b97ba8da
3arthur6 <arthur.trouillot@gmail.com>
2021-07-07 17:09:45 +0200

    Update postrecoveryboot script: logical partition sanitizer

--- recovery/root/system/bin/postrecoveryboot.sh
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -1,17 +1,19 @@
 #!/sbin/sh
 
-umount /vendor >/dev/null 2>&1
-mount -w /vendor >/dev/null 2>&1
-vendor_blk=`df -k|grep "/vendor"|awk '{print $1}'`
-vendor_size=`df -k|grep $vendor_blk|awk '{print $2}'`
-if ! `dd if=/dev/zero of=/vendor/test bs=1 count=1 >/dev/null 2>&1` ; then
-  rm -f /vendor/test >/dev/null 2>&1
-  umount /vendor >/dev/null 2>&1
-  e2fsck -fp $vendor_blk >/dev/null 2>&1
-  resize2fs $vendor_blk $(($vendor_size+10240))K >/dev/null 2>&1
-  e2fsck -p -E unshare_blocks $vendor_blk >/dev/null 2>&1
-  e2fsck -fp $vendor_blk >/dev/null 2>&1
-else
-  rm -f /vendor/test >/dev/null 2>&1
-  umount /vendor >/dev/null 2>&1
-fi
+echo " - Postrecoveryboot script : Logical partition sanitizer"
+for i in 0 1 2 3 ; do
+  mount -w /dev/block/dm-$i
+  mount_pt=`df -k|grep "/dev/block/dm-$i"|awk '{print $6}'`
+  blk_used=`df -k|grep $mount_pt|awk '{print $3}'`
+  blk_full=`df -k|grep $mount_pt|awk '{print $2}'`
+  umount $mount_pt
+  if [ $(($blk_full-$blk_used)) -le 10240 ] ; then
+    echo " - Postrecoveryboot script : Resizing $mount_pt partition (+10MB)"
+    e2fsck -fp /dev/block/dm-$i
+    resize2fs /dev/block/dm-$i $(($blk_full+10240))K
+  fi
+  echo " - Postrecoveryboot script : Forcing $mount_pt partition to be R/W"
+  e2fsck -p -E unshare_blocks /dev/block/dm-$i
+  e2fsck -fp /dev/block/dm-$i
+done
+echo " - Postrecoveryboot script : Done!"
